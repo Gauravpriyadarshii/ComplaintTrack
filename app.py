49bbafbd0690c235fb9e419faf8ccf6bc2fc96b9
Gauravpriyadarshii <146010973+Gauravpriyadarshii@users.noreply.github.com>
2024-03-15 19:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def create_superuser():
         db.session.commit()
         return "Superuser created successfully!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()#(debug=False,host='0.0.0.0')
